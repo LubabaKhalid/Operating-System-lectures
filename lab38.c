c12424880bdc153f22bf222e820ce34e408e3d30
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-11-09 22:24:44 +0500

    added lab3 task8

---
+++ lab38.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+int main(){
+int cpid;
+cpid = fork();
+switch(cpid)
+{
+case 0:
+printf("I am not a zombie process"); break;
+default:
+while(1);
+}
+} 
