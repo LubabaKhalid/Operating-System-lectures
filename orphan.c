f94133647b6e1e0a3802e3995667bd656308e6c0
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-11-12 00:40:51 +0500

    fork c code

---
+++ orphan.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main(){ 
+int cpid = fork(); 
+if (cpid == 0){ 
+printf("Running child, PID=%ld PPID=%ld\n", 
+(long)getpid(), (long)getppid());
+while(1); 
+}
+else
+printf("Terminating parent, PID=%ld PPID=%ld\n",
+(long)getpid(), (long)getppid()); 
+exit(0); 
+return 0;
+}
