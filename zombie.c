152b91f486a8286643b9716291fc0269170d19f2
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-11-12 00:41:25 +0500

    fork c code

---
+++ zombie.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main(){ 
+int cpid = fork(); 
+if (cpid == 0){ 
+printf("Terminating child with PID = %ld\n", 
+(long)getpid()); 
+exit (0); 
+} 
+else{ 
+printf("Running parent, PID=%ld\n",(long)getpid()); 
+while(1); 
+} 
+return 0;
+}
